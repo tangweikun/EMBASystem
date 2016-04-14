f8e56067cd04823f1ddbd785eb7d244b50ded1a5
tangweikun <819573105@qq.com>
2016-04-14 15:52:46 +0800

    fix

--- Manager/client/component/score/scoreItem.jsx
+++ Manager/client/component/score/scoreItem.jsx
@@ -4,12 +4,17 @@ ScoreItem = React.createClass({
   getMeteorData() {
     let sub = Meteor.subscribe('score')
     let score = Score.findOne({studentId: this.props.studentId})
-    console.log("message--",score);
+    // console.log("message--",score);
 
     return {
       score: sub.ready() ? score : null,
       isReady: sub.ready()
     }
+  },
+
+  _txtBlur(e) {
+    let score = e.target.value
+    Meteor.call('addScore2',this.props.studentId,this.props.courseName,this.props.courseId,score)
   },
   render() {
     let m = this.props.courseId
@@ -18,7 +23,7 @@ ScoreItem = React.createClass({
     }
 
     console.log("000",m);
-    console.log("message",this.data.score);
+
     console.log("----",this.data.score ? this.data.score.className : '222');
 
 
@@ -26,12 +31,11 @@ ScoreItem = React.createClass({
     return (
       <div className="col-md-9">
           <div className="col-md-2">{this.props.courseName}</div>
-          <div className="col-md-2">twk</div>
-
+          <div className="col-md-2">{this.data.score.studentName}</div>
           <div className="col-md-2">{this.props.studentId}</div>
           <div className="col-md-2">{this.data.score.className}</div>
-
           <div className="col-md-2">{this.data.score.trainingPlan[m].state}</div>
+          <div className="col-md-2"><input type="text" onBlur={this._txtBlur}/></div>
       </div>
     )
   }
