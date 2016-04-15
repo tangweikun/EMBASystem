688d1550e25a361a6c50f2657a94b088de7eb757
tangweikun <819573105@qq.com>
2016-04-15 20:42:02 +0800

    e

--- Manager/server/method/commit.js
+++ Manager/server/method/commit.js
@@ -1,10 +1,7 @@
 Meteor.methods({
   approval: function(studentId, courseId, state) {
     Commit.update({studentId: studentId, courseId: courseId},{$set: {state: state}})
+  }
 
     })
-
-  },
-
-
-})
+///schedule表和score标
