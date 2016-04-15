ecf86339cfd6e38028ffe548a61ac978b7378637
tangweikun <819573105@qq.com>
2016-04-15 12:05:52 +0800

    show schedule in student

--- Student/client/component/shedule/showShedule.jsx
+++ Student/client/component/shedule/showShedule.jsx
@@ -3,10 +3,48 @@ ShowSchedule = React.createClass({
 
   getMeteorData() {
     let sub = Meteor.subscribe('schedule');
-    let schedule = Schedule.find({}).fetch();
+    let schedule = ''
+    let t1 = ''
+    let count
+    let t3 = ''
+    let all = []
+    let detailsList = []
+    let count1
+    let count2
+    let all2 = []
+
+
+    if (sub.ready()) {
+      schedule = Schedule.find({}).fetch();
+      count1 = schedule.length
+      for (let i = 0; i < count1; i++) {
+        detailsList.push(schedule[i].details)
+      }
+      count2 = detailsList.length
+      for (let j = 0; j < count2; j++) {
+        all = [...all, ...detailsList[j]]
+      }
+      console.log("message",all);
+
+
+//只选择属于该学生的课程
+      let len = all.length
+      for (let j = 0; j < len; j++) {
+        let ss = all[j].studentList
+        console.log("ss",ss);
+        let studentId = Meteor.users.findOne({_id: Meteor.userId()}).profile.studentId
+        console.log("sid",studentId);
+        if (ss.indexOf(studentId) != "-1") {
+          console.log("ssssss",ss);
+          all2 = [...all2, ...all[j]]
+        }
+      }
+      console.log(">>>",all2);
+    }
+
 
     return {
-      schedule:sub.ready() ? schedule : null
+      schedule:sub.ready() ? all2 : null
     }
   },
 
