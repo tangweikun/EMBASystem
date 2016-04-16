7168d29b20cdab90e58ed326ecec58056b209954
tangweikun <819573105@qq.com>
2016-04-16 13:49:38 +0800

    change schedule

--- Manager/server/method/commit.js
+++ Manager/server/method/commit.js
@@ -1,6 +1,27 @@
 Meteor.methods({
   approval: function(studentId, courseId, state) {
     Commit.update({studentId: studentId, courseId: courseId},{$set: {state: state}})
+  },
+
+
+//修改schedule
+  chanSch: function(studentId,courseId) {
+    let schedule = Schedule.findOne({courseId: courseId})
+    let className = Meteor.users.findOne({"profile.studentId": studentId}).profile.theClass
+    details = schedule.details
+    let scheduleIdList = []
+    details.map(function(a,n){
+      if (scheduleIdList.indexOf(a.scheduleId) == '-1') {
+        scheduleIdList.push(a.scheduleId)
+        let det = {}
+        det.details = a
+        det.details.className = className
+        det.details.studentList = [studentId]
+        Schedule.update({ courseId: courseId},{ $push: det}, {upsert: true})
+      }
+    })
+
+
   }
 
     })
