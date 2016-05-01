4e38ae7616b8e5000cc311a19af1df0430918762
tangweikun <819573105@qq.com>
2016-05-01 14:28:59 +0800

    header

--- Manager/client/header.jsx
+++ Manager/client/header.jsx
@@ -1,11 +1,18 @@
 Header = React.createClass({
   render() {
+    const { AppBar } = MUI
+    const screenWidth = window.innerWidth
     let header = {
-      background: 'red',
-      height: '50px',
+      // background: 'red',
+      // height: '50px',
     }
     return (
-      <div style={header} className="row">河北工业大学EMBA综合管理系统</div>
+      <div style={header} className="row">
+        <AppBar
+          title="河北工业大学EMBA综合管理系统"
+          showMenuIconButton={false}
+          />
+        </div>
     );
   }
 });
