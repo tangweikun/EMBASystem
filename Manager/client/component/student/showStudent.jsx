83f7a2a5a3e5b13129ba58843337f46f2b426803
tangweikun <819573105@qq.com>
2016-05-02 13:35:39 +0800

    student

--- Manager/client/component/student/showStudent.jsx
+++ Manager/client/component/student/showStudent.jsx
@@ -2,37 +2,89 @@ ShowStudent = React.createClass({
   mixins: [ReactMeteorData],
 
   getMeteorData() {
-    let sub = Meteor.subscribe("users");
+    let sub = Meteor.subscribe("users")
     let student = Meteor.users.find({roles: 'student'}).fetch()
     return {
-      student: sub.ready() ? student : null
+      student: sub.ready() ? student : null,
+      ready: sub.ready(),
     }
   },
 
   renderstudent() {
-    return this.data.student ? this.data.student.map(function(a,n){
-      return <StudentItem student={a} key={n} />
-    }) : null
+    const { List, ListItem, ActionGrade, ContentInbox, ContentDrafts, RaisedButton, ContentSend, Avatar, Divider } = MUI
+    const styles = {
+      span1: {
+        marginRight: '8px',
+        // width: '20px',
+      },
+      span2: {
+        marginRight: '36px',
+        // width: ''
+      },
+      span3: {
+        marginRight: '20px',
+        // width: '140px',
+      },
+      div1: {
+        marginTop: '1px',
+      },
+    }
+    return this.data.student.map(function(a,n){
+      return (
+        <div key={n} className='col-md-9'>
+        <ListItem
+          primaryText={a.profile.studentName}
+          secondaryText={
+            <div>
+            <div style={styles.div1}>
+              <span style={styles.span1}>班级</span>
+              <span style={styles.span2}>{a.profile.theClass}</span>
+              <span style={styles.span1}>学号</span>
+              <span style={styles.span2}>{a.profile.studentId}</span>
+              <span style={styles.span1}>性别</span>
+              <span style={styles.span2}>{a.profile.gender}</span>
+              <span style={styles.span1}>民族</span>
+              <span style={styles.span3}>{a.profile.nation}</span>
+              <span style={styles.span1}>单位</span>
+              <span style={styles.span3}>{a.profile.organization}</span>
+            </div>
+            <div style={styles.div1}>
+              <span style={styles.span1}>党派</span>
+              <span style={styles.span2}>{a.profile.politicalLandscape}</span>
+              <span style={styles.span1}>类别</span>
+              <span style={styles.span2}>{a.profile.category}</span>
+              <span style={styles.span1}>录取日期</span>
+              <span style={styles.span2}>{a.profile.admissionDate}</span>
+              <span style={styles.span1}>职务</span>
+              <span style={styles.span3}>{a.profile.position}</span>
+            </div>
+          </div>
+          }
+          secondaryTextLines={2}
+        />
+      <Divider />
+        </div>
+      )
+    })
   },
 
   render() {
+    if (!this.data.ready) return null
+    const styles = {
+      titleStyle: {
+        fontSize: '22px',
+        marginTop: '20px',
+        marginBottom: '10px',
+      },
+    }
     return (
       <div>
-        <div className="col-md-9">
-        <div className="col-md-1">学号</div>
-        <div className="col-md-1">姓名</div>
-        <div className="col-md-1">性别</div>
-        <div className="col-md-1">身份证号</div>
-        <div className="col-md-1">民族</div>
-        <div className="col-md-1">党派</div>
-        <div className="col-md-1">公司</div>
-        <div className="col-md-1">职务</div>
-        <div className="col-md-1">录取时间</div>
-        <div className="col-md-1">班级</div>
-        <div className="col-md-1">类别</div>
-        <div className="col-md-1">所在区域</div>
+        <div className="col-md-9" style={styles.titleStyle}>
+          <label>学员列表</label>
         </div>
-        {this.renderstudent()}
+        <div>
+          {this.renderstudent()}
+        </div>
       </div>
     )
   }
--- Manager/client/component/teacher/showTeacher.jsx
+++ Manager/client/component/teacher/showTeacher.jsx
@@ -13,40 +13,42 @@ ShowTeacher = React.createClass({
   renderTeacher() {
     const { List, ListItem, ActionGrade, ContentInbox, ContentDrafts, RaisedButton, ContentSend, Avatar, Divider } = MUI
     const styles = {
-      label1: {
+      span1: {
         marginRight: '8px',
         // width: '20px',
       },
-      label2: {
+      span2: {
         marginRight: '36px',
         // width: ''
       },
-      label3: {
+      span3: {
         marginRight: '20px',
         width: '140px',
       },
       div1: {
-        marginTop: '10px',
+        marginTop: '1px',
       },
     }
     return this.data.teacher.map(function(a,n){
       return (
-        <div key={n}>
+        <div key={n} className='col-md-9'>
         <ListItem
           primaryText={a.teacherName}
           secondaryText={
             <div>
             <div style={styles.div1}>
-              <label style={styles.label1}>性别</label>
-              <label style={styles.label2}>{a.gender}</label>
-              <label style={styles.label1}>学位</label>
-              <label style={styles.label2}>{a.degree}</label>
-              <label style={styles.label1}>职称</label>
-              <label style={styles.label2}>{a.title}</label>
-              <label style={styles.label1}>单位</label>
-              <label style={styles.label3}>{a.organization}</label>
-              <label style={styles.label1}>主讲课程</label>
-              <label style={styles.label3}>{a.course}</label>
+              <span style={styles.span1}>性别</span>
+              <span style={styles.span2}>{a.gender}</span>
+              <span style={styles.span1}>学位</span>
+              <span style={styles.span2}>{a.degree}</span>
+              <span style={styles.span1}>职称</span>
+              <span style={styles.span2}>{a.title}</span>
+              <span style={styles.span1}>主讲课程</span>
+              <span style={styles.span3}>{a.course}</span>
+            <div>
+              <span style={styles.span1}>单位</span>
+              <span style={styles.span3}>{a.organization}</span>
+            </div>
             </div>
           </div>
           }
@@ -60,9 +62,21 @@ ShowTeacher = React.createClass({
 
   render() {
     if (!this.data.ready) return null
+    const styles = {
+      titleStyle: {
+        fontSize: '22px',
+        marginTop: '20px',
+        marginBottom: '10px',
+      },
+    }
     return (
       <div>
-        {this.renderTeacher()}
+        <div className="col-md-9" style={styles.titleStyle}>
+          <span>教师列表</span>
+        </div>
+        <div>
+          {this.renderTeacher()}
+        </div>
       </div>
     )
   }
